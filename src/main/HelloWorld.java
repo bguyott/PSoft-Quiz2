a740944f2ac4b507c1b23956203298b9faff33d6
JooseMann <justinkarate76@gmail.com>
2026-09-11 18:52:35 -0400

    Updated HelloWorld.java to print a given name.

--- src/main/HelloWorld.java
+++ src/main/HelloWorld.java
@@ -2,11 +2,15 @@ package main;
 
 public class HelloWorld {
     public static void main(String[] args) {
-        System.out.println("Hello World!");
+        printHello("John Doe");
 
         for (int i = 1; i <= 100; ++i) {
             System.out.println(i);
         }
 
+    }
+
+    private static void printHello(String name) {
+        System.out.println("Hello " + name + "!");
     }
 }
